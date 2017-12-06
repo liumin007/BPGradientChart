f0a9f94cbddac7678cd7a39f3758aa39e69207ca
liumin007 <511692751@qq.com>
2017-12-06 10:03:35 +0800

    init

---
+++ BPGradientChart/BPGradientChart/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BPGradientChart
+//
+//  Created by liumin on 2017/12/6.
+//  Copyright © 2017年 Gomtel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BPGradientChart/BPGradientChart/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  BPGradientChart
+//
+//  Created by liumin on 2017/12/6.
+//  Copyright © 2017年 Gomtel. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
